c6a431220375c8dc1f0ce8a6d3c0f4fe15213c66
devdelucca <deluccaby@gmail.com>
2022-05-18 15:32:06 -0300

    Impar ou Par

---
+++ pythonExercicios/exerciciosSegundaEtapa/imparOuPar.py
@@ -0,0 +1,6 @@
+numero = int(input('Informe o digito: '))
+resultado = numero % 2
+if resultado ==0:
+    print('o resultado foi de {} foi par'.format(numero))
+else:
+    print('O resultado para o digito {} foi Impar'.format(numero))
