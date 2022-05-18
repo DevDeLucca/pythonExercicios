e57ea1d4639b4de723ef271943e0fd5476927d1d
devdelucca <deluccaby@gmail.com>
2022-05-18 18:30:54 -0300

    Analise de alistamento militar

---
+++ pythonExercicios/exerciciosTerceiraEtapa/alistamentoMilitar.py
@@ -0,0 +1,13 @@
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Entre com sua data de nascimento yyyy: '))
+idade = atual - nascimento
+print('Quem nasceu no ano de {} tem a idade {} no ano atual{}'.format(nascimento, idade, atual))
+if idade == 18:
+    print('Você precisa se alistar nesse ano!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Você não possui idade suficiente para se alistar'.format(saldo))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você ja deveria ter se alistado há {} anos'.format(saldo))
