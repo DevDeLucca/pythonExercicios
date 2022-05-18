8f40fb26b8e3ac8a94246b6b7d652c5047ef1fa3
devdelucca <deluccaby@gmail.com>
2022-05-18 18:22:46 -0300

    Analise do maior digito

---
+++ pythonExercicios/exerciciosTerceiraEtapa/comparadorDeDigitos.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o primeiro digito: '))
+n2 = int(input('Digite o segundo numero: '))
+if n1 > n2:
+    print('O primeiro digito e maior')
+elif n2 > n1:
+    print('O segundo digito e maior')
+else:
+    print('Os digitos possuem mesmos valores')
+
