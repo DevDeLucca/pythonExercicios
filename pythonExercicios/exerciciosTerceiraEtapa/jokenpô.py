d587956991940a20997c71feff1d3ff30481e898
devdelucca <deluccaby@gmail.com>
2022-05-19 10:47:30 -0300

    JOKENPO ADD SLEEP

--- pythonExercicios/exerciciosTerceiraEtapa/jokenpô.py
+++ pythonExercicios/exerciciosTerceiraEtapa/jokenpô.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
 print(''' Suas Opções são:
@@ -6,6 +7,12 @@ print(''' Suas Opções são:
 [ 1 ] Papel
 [ 2 ] Tesoura''')
 jogador = int(input('Qual é sua jogada? '))
+print('JO')
+sleep(1)
+print('kEN')
+sleep(1)
+print('PÔ')
+sleep(1)
 print('--=--' * 15)
 print('Computador Jogou {}'.format(itens[computador]))
 print('Jogador Jogou {}'.format(itens[computador]))
